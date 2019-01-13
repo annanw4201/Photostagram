c917702fbeb11b9992b759867ced0e604dc91469
Wong Tom <23562997+tomwang1024@users.noreply.github.com>
2019-01-12 23:20:58 -0500

    Begin to add chatting features

---
+++ Photostagram/Models/Chat.m
@@ -0,0 +1,22 @@
+//
+//  Chat.m
+//  Photostagram
+//
+//  Created by Wong Tom on 2019-01-12.
+//  Copyright © 2019 Wang Tom. All rights reserved.
+//
+
+#import "Chat.h"
+
+@interface Chat()
+@property(nonatomic)NSString *key;
+@property(nonatomic)NSString *title;
+@property(nonatomic)NSString *memberHashKey;
+@property(nonatomic)NSString *memberUids;
+@property(nonatomic)NSString *lastMessage;
+@property(nonatomic)NSDate *lastMessageSent;
+@end
+
+@implementation Chat
+
+@end
